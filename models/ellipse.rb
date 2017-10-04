770f7f3372fd667b5ee2185be0c7d7fd27408a9f
--get <--get>
2017-10-04 14:54:48 +0200

    Init ellipse.

---
+++ models/ellipse.rb
@@ -0,0 +1,23 @@
+require_relative('../models/conic')
+require_relative('../models/modules/determinator')
+
+class Ellipse < Conic
+  include Determinator
+
+  # Conic
+  # Conic equation type: ax^2 + by^2 + dx + ey + f = 0
+  def initialize(x2:, y2:, x:, y:, k:)
+    super(x2: x2, y2: y2, xy: 0, x: x, y: y, k: k)
+    validation
+  end
+
+  # TODO: To test when i can create some circunferences.
+  def == (ellipse)
+  end
+
+  private
+
+  def validation
+  end
+
+end
