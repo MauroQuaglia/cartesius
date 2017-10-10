e99bc89e43222eed32cc77372e843071ccd8eb82
nautilus@cachalot <nautilus@trovaprezzi.it>
2017-10-10 14:49:09 +0200

    Tests new hyperbola.

--- spec/lib/conics/models/hyperbola_spec.rb
+++ spec/lib/conics/models/hyperbola_spec.rb
@@ -8,12 +8,12 @@ describe Conics::Hyperbola do
     describe 'empty set' do
 
       it 'should reject a simple equation' do
-        # x^2 + y^2 / 2 = -1
+        # x^2 + y^2 / 2 = -1 --> 2x^2 + y^2 + 2 = 0
         expect {described_class.new(x2: 2, y2: 1, x: 0, y: 0, k: 2)}.to raise_error(ArgumentError)
       end
 
       it 'should reject a general equation' do
-        # (x + 1)^2 + (y - 1)^2 / 2 = -1
+        # (x + 1)^2 + (y - 1)^2 / 2 = -1 --> 2x^2 + y^2 + 4x -2y + 5 = 0
         expect {described_class.new(x2: 2, y2: 1, x: 4, y: -2, k: 5)}.to raise_error(ArgumentError)
       end
 
@@ -22,12 +22,12 @@ describe Conics::Hyperbola do
     describe 'point' do
 
       it 'should reject a simple equation' do
-        # x^2 + y^2 / 2 = 0
+        # x^2 + y^2 / 2 = 0 --> 2x^2 + y^2 = 0
         expect {described_class.new(x2: 2, y2: 1, x: 0, y: 0, k: 0)}.to raise_error(ArgumentError)
       end
 
       it 'should reject a general equation' do
-        # (x + 1)^2 + (y - 1)^2 / 2 = 0
+        # (x + 1)^2 + (y - 1)^2 / 2 = 0 --> 2x^2 + y^2 + 4x -2y + 3 = 0
         expect {described_class.new(x2: 2, y2: 1, x: 4, y: -2, k: 3)}.to raise_error(ArgumentError)
       end
 
@@ -36,12 +36,12 @@ describe Conics::Hyperbola do
     describe 'line' do
 
       it 'should reject a simple equation' do
-        # x^2 - y^2 = 0 or (x + y)(x - y) = 0
+        # x^2 - y^2 = 0
         expect {described_class.new(x2: 1, y2: -1, x: 0, y: 0, k: 0)}.to raise_error(ArgumentError)
       end
 
       it 'should reject a general equation' do
-        # (x + 1)^2 - (y - 1)^2 / 2 = 0
+        # (x + 1)^2 - (y - 1)^2 / 2 = 0 --> 2x^2 - y^2 + 4x - 2y + 1 = 0
         expect {described_class.new(x2: 2, y2: -1, x: 4, y: -2, k: 1)}.to raise_error(ArgumentError)
       end
 
@@ -50,12 +50,12 @@ describe Conics::Hyperbola do
     describe 'circumference' do
 
       it 'should reject a simple equation' do
-        # x^2 + y^2 = 1
+        # x^2 + y^2 = 1 --> x^2 + y^2 - 1 = 0
         expect {described_class.new(x2: 1, y2: 1, x: 0, y: 0, k: -1)}.to raise_error(ArgumentError)
       end
 
       it 'should reject a general equation' do
-        # (x + 1)^2 + (y - 1)^2 = 1
+        # (x + 1)^2 + (y - 1)^2 = 1 --> x^2 + y^2 + 2x - 2y + 1 = 0
         expect {described_class.new(x2: 1, y2: 1, x: 2, y: -2, k: 1)}.to raise_error(ArgumentError)
       end
 
@@ -64,12 +64,12 @@ describe Conics::Hyperbola do
     describe 'ellipse' do
 
       it 'should accept a simple equation' do
-        # x^2 + y^2 / 2 = 1
+        # x^2 + y^2 / 2 = 1 --> 2x^2 + y^2 - 2 = 0
         expect {described_class.new(x2: 2, y2: 1, x: 0, y: 0, k: -2)}.to raise_error(ArgumentError)
       end
 
       it 'should accept a general equation' do
-        # (x + 1)^2 + (y - 1)^2 / 2 = 1
+        # (x + 1)^2 + (y - 1)^2 / 2 = 1 --> 2x^2 + y^2 + 4x - 2y + 1 = 0
         expect {described_class.new(x2: 2, y2: 1, x: 4, y: -2, k: 1)}.to raise_error(ArgumentError)
       end
 
@@ -78,12 +78,12 @@ describe Conics::Hyperbola do
     describe 'parabola' do
 
       it 'should reject a simple equation' do
-        # y = x^2
+        # y = x^2 --> x^2 - y = 0
         expect {described_class.new(x2: 1, y2: 0, x: 0, y: -1, k: 0)}.to raise_error(ArgumentError)
       end
 
       it 'should reject a general equation' do
-        # y = x^2 + x + 1
+        # y = x^2 + x + 1 --> x^2 + x - y + 1 = 0
         expect {described_class.new(x2: 1, y2: 0, x: 1, y: -1, k: 1)}.to raise_error(ArgumentError)
       end
 
@@ -92,12 +92,12 @@ describe Conics::Hyperbola do
     describe 'hyperbola' do
 
       it 'should accept a simple equation' do
-        # x^2 - y^2  = 1
+        # x^2 - y^2  = 1 --> x^2 - y^ 2 - 1 = 0
         expect {described_class.new(x2: 1, y2: -1, x: 0, y: 0, k: -1)}.not_to raise_error
       end
 
       it 'should accept a general equation' do
-        # (x + 1)^2 - (y - 1)^2 / 2 = -1
+        # (x + 1)^2 - (y - 1)^2 / 2 = -1 --> 2x^2 - y^2 + 4x + 2y + 4 = 0
         expect {described_class.new(x2: 2, y2: -1, x: 4, y: 2, k: 4)}.not_to raise_error
       end
 
