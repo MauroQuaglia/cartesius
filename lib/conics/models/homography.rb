2b19e0490de18bd6c97e3977abff22eb3c51f54e
nautilus@cachalot <nautilus@trovaprezzi.it>
2017-10-11 16:43:54 +0200

    New tests for homography.

--- lib/conics/models/homography.rb
+++ lib/conics/models/homography.rb
@@ -1,3 +1,26 @@
+require_relative('../models/conic')
+
 module Conics
+
+  class Homography < Conic
+
+    # Conic
+    # Conic equation type: xy + dx + ey + f = 0
+    def initialize(x:, y:, k:)
+      super(x2: 0, y2: 0, xy: 1, x: x, y: y, k: k)
+      validation
+    end
+
+    private
+
+    def validation
+
+      if @k_coeff == @x_coeff * @y_coeff
+        coefficients_error
+      end
+
+    end
+
+  end
 
 end
