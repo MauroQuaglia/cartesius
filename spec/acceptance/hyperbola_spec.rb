bd6b0f679fbdf8e4f0f5fa8cd3ec67fd39837ecf
nautilus@cachalot <nautilus@trovaprezzi.it>
2017-11-14 15:22:55 +0100

    Test acceptance for hyperbola.

--- spec/acceptance/hyperbola_spec.rb
+++ spec/acceptance/hyperbola_spec.rb
@@ -25,70 +25,63 @@ describe Cartesius::Hyperbola do
     expect(hyperbola.ascending_asymptote).to eq(line.new(x: 3, y: -4, k: 0))
     expect(hyperbola.descending_asymptote).to eq(line.new(x: 3, y: 4, k: 0))
     expect(hyperbola.to_equation).to eq('+9x^2 -16y^2 -144 = 0')
-
   end
 
-  xit 'should validate the characteristics of a simple equation with focus on y axis' do
-    # x^2 / 9 + y^2 / 25 = 1 --> 25x^2 + 9y^2 - 225 = 0
-    ellipse = described_class.new(x2: 25, y2: 9, x: 0, y: 0, k: -225)
+  it 'should validate the characteristics of a simple equation with focus on y axis' do
+    # x^2/16 - y^2/9 = -1 --> 9x^2 - 16y^2 + 144 = 0
+    hyperbola = described_class.new(x2: 9, y2: -16, x: 0, y: 0, k: 144)
 
-    expect(ellipse.focus1).to eq(Cartesius::Point.new(x: 0, y: 4))
-    expect(ellipse.focus2).to eq(Cartesius::Point.new(x: 0, y: -4))
-    expect(ellipse.focal_distance).to eq(8)
-    expect(ellipse.center).to eq(Cartesius::Point.origin)
-    expect(ellipse.sum_of_distances).to eq(10)
-    expect(ellipse.x_semi_axis_length).to eq(3)
-    expect(ellipse.y_semi_axis_length).to eq(5)
-    expect(ellipse.major_semi_axis).to eq(5)
-    expect(ellipse.minor_semi_axis).to eq(3)
-    expect(ellipse.vertices).to eq ([
-        Cartesius::Point.new(x: 3, y: 0), Cartesius::Point.new(x: 0, y: -5),
-        Cartesius::Point.new(x: -3, y: 0), Cartesius::Point.new(x: 0, y: 5)
-    ])
-    expect(ellipse.eccentricity).to eq(Rational(4, 5))
-    expect(ellipse.to_equation).to eq('+25x^2 +9y^2 -225 = 0')
+    expect(hyperbola.focus1).to eq(point.new(x: 0, y: 5))
+    expect(hyperbola.focus2).to eq(point.new(x: 0, y: -5))
+    expect(hyperbola.focal_axis).to eq(segment.new(extreme1: point.new(x: 0, y: -5), extreme2: point.new(x: 0, y: 5)))
+    expect(hyperbola.distance).to eq(6)
+    expect(hyperbola.center).to eq(point.origin)
+    expect(hyperbola.vertices).to eq([point.new(x: 0, y: -3), point.new(x: 0, y: 3)])
+    expect(hyperbola.transverse_axis).to eq(segment.new(extreme1: point.new(x: 0, y: -3), extreme2: point.new(x: 0, y: 3)))
+    expect(hyperbola.not_transverse_axis).to eq(segment.new(extreme1: point.new(x: -4, y: 0), extreme2: point.new(x: 4, y: 0)))
+    expect(hyperbola.eccentricity).to eq(Rational('5/3'))
+    expect(hyperbola.equilateral?).to be_falsey
+    expect(hyperbola.ascending_asymptote).to eq(line.new(x: 3, y: -4, k: 0))
+    expect(hyperbola.descending_asymptote).to eq(line.new(x: 3, y: 4, k: 0))
+    expect(hyperbola.to_equation).to eq('+9x^2 -16y^2 +144 = 0')
   end
 
-  xit 'should validate the characteristics of a general equation with focus on x axis' do
-    # (x - 1)^2 / 25 + (y - 1)^2 / 9 = 1 --> 9x^2 + 25y^2 - 18x - 50y - 191 = 0
-    ellipse = described_class.new(x2: 9, y2: 25, x: -18, y: -50, k: -191)
+  it 'should validate the characteristics of a general equation with focus on x axis' do
+    # (x - 1)^2/16 - (y - 1)^2/9 = 1 --> 9x^2 - 16y^2 - 18x + 32y - 151 = 0
+    hyperbola = described_class.new(x2: 9, y2: -16, x: -18, y: 32, k: -151)
 
-    expect(ellipse.focus1).to eq(Cartesius::Point.new(x: 5, y: 1))
-    expect(ellipse.focus2).to eq(Cartesius::Point.new(x: -3, y: 1))
-    expect(ellipse.focal_distance).to eq(8)
-    expect(ellipse.center).to eq(Cartesius::Point.new(x: 1, y: 1))
-    expect(ellipse.sum_of_distances).to eq(10)
-    expect(ellipse.x_semi_axis_length).to eq(5)
-    expect(ellipse.y_semi_axis_length).to eq(3)
-    expect(ellipse.major_semi_axis).to eq(5)
-    expect(ellipse.minor_semi_axis).to eq(3)
-    expect(ellipse.vertices).to eq ([
-        Cartesius::Point.new(x: 6, y: 1), Cartesius::Point.new(x: 1, y: -2),
-        Cartesius::Point.new(x: -4, y: 1), Cartesius::Point.new(x: 1, y: 4)
-    ])
-    expect(ellipse.eccentricity).to eq(Rational(4, 5))
-    expect(ellipse.to_equation).to eq('+9x^2 +25y^2 -18x -50y -191 = 0')
+    expect(hyperbola.focus1).to eq(point.new(x: 6, y: 1))
+    expect(hyperbola.focus2).to eq(point.new(x: -4, y: 1))
+    expect(hyperbola.focal_axis).to eq(segment.new(extreme1: point.new(x: -4, y: 1), extreme2: point.new(x: 6, y: 1)))
+    expect(hyperbola.distance).to eq(8)
+    expect(hyperbola.center).to eq(point.new(x: 1, y: 1))
+    expect(hyperbola.vertices).to eq([point.new(x: -3, y: 1), point.new(x: 5, y: 1)])
+    expect(hyperbola.transverse_axis).to eq(segment.new(extreme1: point.new(x: -3, y: 1), extreme2: point.new(x: 5, y: 1)))
+    expect(hyperbola.not_transverse_axis).to eq(segment.new(extreme1: point.new(x: 1, y: -2), extreme2: point.new(x: 1, y: 4)))
+    expect(hyperbola.eccentricity).to eq(Rational('5/4'))
+    expect(hyperbola.equilateral?).to be_falsey
+    expect(hyperbola.ascending_asymptote).to eq(line.new(x: 3, y: -4, k: 1))
+    expect(hyperbola.descending_asymptote).to eq(line.new(x: 3, y: 4, k: -7))
+    expect(hyperbola.to_equation).to eq('+9x^2 -16y^2 -18x +32y -151 = 0')
   end
 
-  xit 'should validate the characteristics of a general equation with focus on y axis' do
-    # (x - 1)^2 / 9 + (y - 1)^2 / 25 = 1 --> 25x^2 + 9y^2 - 50x - 18y - 191 = 0
-    ellipse = described_class.new(x2: 25, y2: 9, x: -50, y: -18, k: -191)
+  it 'should validate the characteristics of a general equation with focus on y axis' do
+    # (x - 1)^2/16 - (y - 1)^2/9 = -1 --> 9x^2 - 16y^2 - 18x + 32y + 137 = 0
+    hyperbola = described_class.new(x2: 9, y2: -16, x: -18, y: 32, k: 137)
 
-    expect(ellipse.focus1).to eq(Cartesius::Point.new(x: 1, y: 5))
-    expect(ellipse.focus2).to eq(Cartesius::Point.new(x: 1, y: -3))
-    expect(ellipse.focal_distance).to eq(8)
-    expect(ellipse.center).to eq(Cartesius::Point.new(x: 1, y: 1))
-    expect(ellipse.sum_of_distances).to eq(10)
-    expect(ellipse.x_semi_axis_length).to eq(3)
-    expect(ellipse.y_semi_axis_length).to eq(5)
-    expect(ellipse.major_semi_axis).to eq(5)
-    expect(ellipse.minor_semi_axis).to eq(3)
-    expect(ellipse.vertices).to eq ([
-        Cartesius::Point.new(x: 4, y: 1), Cartesius::Point.new(x: 1, y: -4),
-        Cartesius::Point.new(x: -2, y: 1), Cartesius::Point.new(x: 1, y: 6)
-    ])
-    expect(ellipse.eccentricity).to eq(Rational(4, 5))
-    expect(ellipse.to_equation).to eq('+25x^2 +9y^2 -50x -18y -191 = 0')
+    expect(hyperbola.focus1).to eq(point.new(x: 1, y: 6))
+    expect(hyperbola.focus2).to eq(point.new(x: 1, y: -4))
+    expect(hyperbola.focal_axis).to eq(segment.new(extreme1: point.new(x: 1, y: -4), extreme2: point.new(x: 1, y: 6)))
+    expect(hyperbola.distance).to eq(6)
+    expect(hyperbola.center).to eq(point.new(x: 1, y: 1))
+    expect(hyperbola.vertices).to eq([point.new(x: 1, y: -2), point.new(x: 1, y: 4)])
+    expect(hyperbola.transverse_axis).to eq(segment.new(extreme1: point.new(x: 1, y: -2), extreme2: point.new(x: 1, y: 4)))
+    expect(hyperbola.not_transverse_axis).to eq(segment.new(extreme1: point.new(x: -3, y: 1), extreme2: point.new(x: 5, y: 1)))
+    expect(hyperbola.eccentricity).to eq(Rational('5/3'))
+    expect(hyperbola.equilateral?).to be_falsey
+    expect(hyperbola.ascending_asymptote).to eq(line.new(x: 3, y: -4, k: 1))
+    expect(hyperbola.descending_asymptote).to eq(line.new(x: 3, y: 4, k: -7))
+    expect(hyperbola.to_equation).to eq('+9x^2 -16y^2 -18x +32y +137 = 0')
   end
 
 end
