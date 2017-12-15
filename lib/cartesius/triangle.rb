9fe7cdd3b212ac6f19b266d68261c258972e87b2
nautilus@cachalot <nautilus@trovaprezzi.it>
2017-12-15 14:58:50 +0100

    Test on triangle.

--- lib/cartesius/triangle.rb
+++ lib/cartesius/triangle.rb
@@ -1,22 +1,39 @@
 require('cartesius/line')
+require('cartesius/segment')
 
 module Cartesius
 
   class Triangle
-    attr_reader(:v1, :v2, :v3)
+    attr_reader(:vertex1, :vertex2, :vertex3)
+    attr_reader(:side12, :side13, :side23)
 
-    def initialize(v1:, v2:, v3:)
-      @v1, @v2, @v3 = v1, v2, v3
-      validation
+    def initialize(vertex1:, vertex2:, vertex3:)
+      validation(vertex1, vertex2, vertex3)
+      @vertex1, @vertex2, @vertex3 = vertex1, vertex2, vertex3
+      @side12 = Segment.new(extreme1: @vertex1, extreme2: @vertex2)
+      @side13 = Segment.new(extreme1: @vertex1, extreme2: @vertex3)
+      @side23 = Segment.new(extreme1: @vertex2, extreme2: @vertex3)
+    end
+
+    def vertices
+      [@vertex1, @vertex2, @vertex3]
+    end
+
+    def sides
+      [@side12, @side13, @side23]
+    end
+
+    def congruent?(triangle)
+      triangle.instance_of?(self.class)
     end
 
     private
 
-    def validation
-      if @v1 == @v2 or @v1 == @v3 or @v2 == @v3
+    def validation(vertex1, vertex2, vertex3)
+      if vertex1 == vertex2 or vertex1 == vertex3 or vertex2 == vertex3
         raise ArgumentError.new('Vertices must be different!')
       end
-      if Line.by_points(point1: @v1, point2: @v2).include?(@v3)
+      if Line.by_points(point1: vertex1, point2: vertex2).include?(vertex3)
         raise ArgumentError.new('Vertices must not be aligned!')
       end
     end
